1c918c83563ffd2521eb4bda46d3303cea9a64d9
matematikbahcem <matematikbahcemm@gmail.com>
2024-03-05 12:57:25 +0100

    add gtag for analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  modules: ["nuxt-gtag"],
+  gtag: {
+    id: "G-RY20N1K2GF",
+  },
   devtools: { enabled: true },
   css: ["~/assets/css/main.css"],
   postcss: {
